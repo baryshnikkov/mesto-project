5c25fad83224a6aadbefb3ee9860d319b3c6c2dd
Maxim Baryshnikov <maximbaryshnikov1997@yandex.ru>
2022-07-26 23:09:34 +0300

    feat: добавить popup добавления новой карточки с рабочим функционалом

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 const buttonEditProfile = document.querySelector('.profile__edit-button');
-const buttonPopupClose = document.querySelector('.popup__in-button-exit');
-const popup = document.querySelector('.popup');
+const popupEdit = document.querySelector('#popup-edit');
+const buttonPopupEditClose = popupEdit.querySelector('.popup__in-button-exit');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
-const profileNameInput = document.querySelector('.popup__in-text_el_name');
-const profileProfessionInput = document.querySelector('.popup__in-text_el_profession');
-const formElement = document.querySelector('.popup__container');
+const profileNameInputEdit = popupEdit.querySelector('.popup__in-text_el_name');
+const profileProfessionInputEdit = popupEdit.querySelector('.popup__in-text_el_profession');
+const formElement = popupEdit.querySelector('.popup__container');
 
+buttonEditProfile.addEventListener('click', function () {
+  popupEdit.classList.add('popup_opened');
+  profileNameInputEdit.value = profileName.textContent;
+  profileProfessionInputEdit.value = profileProfession.textContent;
+});
 
-function popupOpene() {
-  popup.classList.add('popup_opened');
-  profileNameInput.value = profileName.textContent;
-  profileProfessionInput.value = profileProfession.textContent;
+function popupClose(name) {
+  name.classList.remove('popup_opened');
 }
-buttonEditProfile.addEventListener('click', popupOpene);
-
-function popupClose() {
-  popup.classList.remove('popup_opened');
-}
-buttonPopupClose.addEventListener('click', popupClose);
+buttonPopupEditClose.addEventListener('click', function () {
+  popupClose(popupEdit);
+});
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  profileName.textContent = profileNameInput.value;
-  profileProfession.textContent = profileProfessionInput.value;
-  popupClose();
+  profileName.textContent = profileNameInputEdit.value;
+  profileProfession.textContent = profileProfessionInputEdit.value;
+  popupClose(popupEdit);
 }
 formElement.addEventListener('submit', formSubmitHandler);
+
 
 const initialCards = [
   {
@@ -55,7 +56,7 @@ const initialCards = [
   }
 ];
 
-function addCard(src) {
+function addCards(src) {
   const templateCard = document.querySelector('#card').content;
   const sectionCard = document.querySelector('.cards');
 
@@ -68,4 +69,38 @@ function addCard(src) {
   });
 }
 
-addCard(initialCards);
+addCards(initialCards);
+
+
+const buttonAddProfile = document.querySelector('.profile__add-button');
+const popupAdd = document.querySelector('#popup-create');
+const buttonPopupAddClose = popupAdd.querySelector('.popup__in-button-exit');
+const profileNameInputAdd = popupAdd.querySelector('.popup__in-text_el_name');
+const profileProfessionInputAdd = popupAdd.querySelector('.popup__in-text_el_profession');
+const formCard = popupAdd.querySelector('.popup__container');
+
+buttonAddProfile.addEventListener('click', function () {
+  popupAdd.classList.add('popup_opened');
+});
+
+buttonPopupAddClose.addEventListener('click', function () {
+  popupClose(popupAdd);
+});
+
+function addNewCard(evt) {
+  evt.preventDefault();
+  const templateCard = document.querySelector('#card').content;
+  const sectionCard = document.querySelector('.cards');
+  const newCard = templateCard.querySelector('.card').cloneNode(true);
+  newCard.querySelector('.card__photo').src = profileProfessionInputAdd.value;
+  newCard.querySelector('.card__photo').alt = profileNameInputAdd.value;
+  newCard.querySelector('.card__title').textContent = profileNameInputAdd.value;
+  sectionCard.prepend(newCard);
+  const obj = {};
+  obj.name = profileNameInputAdd.value;
+  obj.link = profileProfessionInputAdd.value;
+  initialCards.unshift(obj);
+  popupClose(popupAdd);
+}
+
+formCard.addEventListener('submit', addNewCard);
